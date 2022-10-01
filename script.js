8b2c8657182b51026f407958f27283cc922a54c2
RomanDmsh <the_seeker@ukr.net>
2022-10-01 16:01:18 +0300

    Browser UI added

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function getComputerChoice() {
 	if (rndNumber >= 0.33 && rndNumber <= 0.66) return 'Paper';
 	else return 'Scissors';
 }
-function getPlayerChoice() {
-	let choiceNumber = prompt(
-		'Enter your choice, 1 for Rock, 2 for Paper, 3 for Scissors, default is 1'
-	);
-	if (choiceNumber === '1') return 'Rock';
-	if (choiceNumber === '2') return 'Paper';
-	if (choiceNumber === '3') return 'Scissors';
-	else return 'Rock';
-}
+// function getPlayerChoice() {
+// 	let choiceNumber = prompt(
+// 		'Enter your choice, 1 for Rock, 2 for Paper, 3 for Scissors, default is 1'
+// 	);
+// 	if (choiceNumber === '1') return 'Rock';
+// 	if (choiceNumber === '2') return 'Paper';
+// 	if (choiceNumber === '3') return 'Scissors';
+// 	else return 'Rock';
+// }
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection === 'Rock') {
 		if (computerSelection === 'Rock') return 'Even';
@@ -36,6 +36,42 @@ function playRound(playerSelection, computerSelection) {
 			return `You win! ${playerSelection} beats ${computerSelection}`;
 	}
 }
-for (let i = 0; i < 10; i++) {
-	console.log(playRound(getPlayerChoice(), getComputerChoice()));
+// for (let i = 0; i < 10; i++) {
+// 	console.log(playRound(getPlayerChoice(), getComputerChoice()));
+// }
+const result = document.querySelector('#result');
+const score = document.querySelector('#score');
+const total = document.querySelector('#total');
+let compPoints = 0;
+let playerPoints = 0;
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+	button.addEventListener('click', () => {
+		if (total.innerText === '') {
+			result.innerText = playRound(button.innerText, getComputerChoice());
+			scoreCalculate();
+			scoreUpdate();
+			totalCheck();
+		}
+	});
+});
+
+function scoreCalculate() {
+	if (result.innerText.slice(4, 5) === 'w') {
+		playerPoints++;
+	}
+	if (result.innerText.slice(4, 5) === 'l') {
+		compPoints++;
+	}
+}
+function scoreUpdate() {
+	score.innerText = `Player ${playerPoints} : Computer ${compPoints}`;
+}
+function totalCheck() {
+	if (playerPoints === 5) {
+		total.innerText = 'PLAYER is the winner of the game!';
+	} else if (compPoints === 5) {
+		total.innerText = 'COMPUTER is the winner of the game!';
+	}
 }
